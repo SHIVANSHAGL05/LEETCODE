54fa88f8032bfbba2157791e78c9dd63b790a4bb
SHIVANSHAGL05 <149140668+SHIVANSHAGL05@users.noreply.github.com>
2025-03-13 14:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (58.28%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+
+        return dummy.next;
+    }
+    public static ListNode arrayToList(int[] arr) {
+        if (arr.length == 0) return null;
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+        for (int i = 1; i < arr.length; i++) {
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = 2;
+        
+        ListNode head = arrayToList(arr);
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = sol.removeNthFromEnd(head, n);
+        System.out.print("Modified List: ");
+        printList(head);
+    }
+}
+
+
