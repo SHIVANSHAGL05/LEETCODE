cef64201a9a3a0e709e6b518cf4f11f892717d77
SHIVANSHAGL05 <149140668+SHIVANSHAGL05@users.noreply.github.com>
2025-03-30 09:57:50 +0530

    Time: 9 ms (39.82%) | Memory: 43 MB (10.37%) - LeetSync

---
+++ 768-partition-labels/partition-labels.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex.put(s.charAt(i), i);
+        }
+
+        List<Integer> partitions = new ArrayList<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastIndex.get(s.charAt(i))); 
+            if (i == end) {
+                partitions.add(end - start + 1);
+                start = i + 1; 
+            }
+        }
+
+        return partitions;
+    }
+}
